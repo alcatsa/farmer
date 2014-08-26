88c5fe92abb2e84e2631d185404a0e565740b211
alcatsa <alcatsa81@gmail.com>
2014-08-26 22:29:54 +0200

    Revert "Update"
    This reverts commit feda2d8b9f66bbed0a21c182df2253f7ccfe417e.

--- src/main/java/net/counterearth/farmer/proxy/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package net.counterearth.farmer.proxy;
-
-/**
- * Created by Andreas Sjöstedt on 2014-08-26.
- */
-public abstract class CommonProxy implements IProxy
-{
-}
--- src/main/java/net/counterearth/farmer/proxy/IProxy.java
+++
@@ -1,8 +0,0 @@
-package net.counterearth.farmer.proxy;
-
-/**
- * Created by Andreas Sjöstedt on 2014-08-26.
- */
-public interface IProxy
-{
-}
